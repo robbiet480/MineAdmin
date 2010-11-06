dabbdcec549a80e342fbf1421a596c7a9591ddb7
Robbie Trencheny <me@robbiet.us>
2010-11-05 20:53:50 -0700

    updated path in cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repository,  "git@github.com:robbiet480/MineAdmin.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
