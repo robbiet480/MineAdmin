fe655bfebe9d38eb8126ada234feeeccaa1f0ac8
Robbie Trencheny <me@robbiet.us>
2010-11-05 22:10:41 -0700

    moved menu into includes/header.php

--- start.php
+++ start.php
@@ -1,37 +1,7 @@
 <?php
 require_once('header_inc.php');
+require_once('includes/header.php');
 ?>
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<title>Administration Index</title>
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.alerts.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.autocomplete.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.jgrowl.css" type="text/css" media="screen" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery-css-transform.js"></script>
-	<script type="text/javascript" src="jquery.autocomplete.pack.js"></script>
-	<script type="text/javascript" src="jquery.bgiframe.min.js"></script>
-	<script type="text/javascript" src="jquery.alerts.js"></script>
-	<script type="text/javascript" src="jquery.jgrowl_minimized.js"></script>
-	<script type="text/javascript" src="jquery.ui.all.js"></script>
-	<script type="text/javascript" src="system.js"></script> 
-</head>
-<body>
-	<div id="menu-wrap">
-		<h2>MineCraft Server</h2>
-		<ul>
-			<li><a href="logout_process.php">Logout</a></li>
-			<li><a href="settings.php">Settings</a></li>
-			<li><a href="backup.php">Backup</a></li>
-			<li><a href="exec.php">Exec</a></li>
-			<li><a href="groups.php">Groups</a></li>
-			<li><a href="bans.php">Bans</a></li>
-			<li><a href="users.php">Users</a></li>
-			<li><a href="start.php">Online Now</a></li>
-		</ul>
-	</div>
 	<div id="page_wrap">
 		<div id="online_wrap">
 			<h1>Who's Online</h1>
