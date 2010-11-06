9f1951a8dc903d9ec073d33c82537381816418b7
Dennis Tang <dtang@ismcreative.net>
2010-11-06 03:24:14 -0700

    final attempt at jquery delay

--- system.js
+++ system.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#page_wrap').delay(250).slideDown(500);
 	$('#menu-wrap a').click(function() {
 		$('#page_wrap').slideUp(500);
-		$(function(){window.location = $(this).attr("href"), 500);
+		$(this).delay(500, function(){window.location = $(this).attr('href');});
 		return false;
 	});
 });	
