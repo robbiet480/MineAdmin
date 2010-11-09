e23b3af0f6e6977ae0259fb85eeb50a7b79f3801
Robbie Trencheny <me@robbiet.us>
2010-11-08 23:05:40 -0800

    moved files around

--- includes/header.php
+++ includes/header.php
@@ -15,25 +15,25 @@ if(count($pid)==1) {
 		echo "<title>MCSAdmin - MinecraftServers.com</title>";
 	}
 	?>
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.alerts.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.autocomplete.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/jquery.alerts.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/jquery.autocomplete.css" type="text/css" media="screen" />
     <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.2.css" media="screen" />
-	<link rel="stylesheet" href="jquery.jgrowl.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/jquery.jgrowl.css" type="text/css" media="screen" />
 	<link rel="apple-touch-icon" href="images/apple-icon.png"/>
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-touch-fullscreen" content="yes" />
 	<script type="text/javascript" src="js/detect.ios.js"></script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery-css-transform.js"></script>
-	<script type="text/javascript" src="jquery.autocomplete.pack.js"></script>
-	<script type="text/javascript" src="jquery.bgiframe.min.js"></script>
-	<script type="text/javascript" src="jquery.alerts.js"></script>
+	<script type="text/javascript" src="js/jquery-css-transform.js"></script>
+	<script type="text/javascript" src="js/jquery.autocomplete.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.bgiframe.min.js"></script>
+	<script type="text/javascript" src="js/jquery.alerts.js"></script>
     <script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 	<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.2.js"></script>
-	<script type="text/javascript" src="jquery.jgrowl_minimized.js"></script>
-	<script type="text/javascript" src="jquery.ui.all.js"></script>
-	<script type="text/javascript" src="system.js"></script> 
+	<script type="text/javascript" src="js/jquery.jgrowl_minimized.js"></script>
+	<script type="text/javascript" src="js/jquery.ui.all.js"></script>
+	<script type="text/javascript" src="js/system.js"></script> 
 </head>
 <body>
 	<div id="menu-wrap">
