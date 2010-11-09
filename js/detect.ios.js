8437d9af5f0c61a3f112181049f8ea2b35de66a7
Dennis Tang <dtang@ismcreative.net>
2010-11-08 23:52:47 -0800

    ios startup fix?

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -1,11 +1,3 @@
 if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
-	meta = document.createElement('meta');
-	meta.name = "viewport";
-	meta.content = "initial-scale = 1.0, user-scalable = no"
-	document.getElementsByTagName('head').item(0).appendChild(meta);
-	link = document.createElement('link');
-	link.rel = "apple-touch-startup-image";
-	link.href = "images/ipad-startup,png"
-	document.getElementsByTagName('head').item(0).appendChild(link);
-	$("input").append(' autocorrect="off" autocapitalize="off" ');
+	document.write("<meta name="viewport" content="initial-scale = 1.0, user-scalable = no" /><link rel="apple-touch-startup-image" href="images/ipad-startup.png" />");
 }
