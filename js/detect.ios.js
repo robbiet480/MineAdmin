97b6672b0634dd5c7e5873d5da12cc6cf87ce06e
Dennis Tang <dtang@ismcreative.net>
2010-11-08 23:23:39 -0800

    secret!

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -3,6 +3,10 @@ if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
 	meta.name = "viewport";
 	meta.content = "initial-scale = 1.0, user-scalable = no"
 	document.getElementsByTagName('head').item(0).appendChild(meta);
+	link = document.createElement('link');
+	link.rel = "apple-touch-startup-image";
+	link.href = "../images/ipad-startup,png"
+	document.getElementsByTagName('head').item(1).appendChild(link);
 	$("input").append(' autocorrect="off" autocapitalize="off" ');
 }
 
