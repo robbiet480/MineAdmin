b079f80e3cef63ed66f154a0f80a063447b87057
Robbie Trencheny <me@robbiet.us>
2010-11-08 19:11:49 -0800

    fixed footer w/ version

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 	<div id="footer">
 		<span id="copyright"><a href="http://hostiio.com/" target="_blank">&copy; 2010 Hostiio, LLC.</a></span>
-		<span id="footer_info"><a href="http://billing.hostiio.com/?systpl=mcs" target="_blank">Support/Billing</a> | Build 1337</span>
+		<span id="footer_info"><a href="http://billing.hostiio.com/?systpl=mcs" target="_blank">Support/Billing</a> | Build <?php echo $GENERAL['version']; ?></span>
 	</div>
 
 </body>
