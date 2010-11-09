faec972359ca7f8df293960ee1ccbb4b07f2c803
Robbie Trencheny <me@robbiet.us>
2010-11-08 23:13:20 -0800

    compressed javascript, wrapped image link in ''

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -1,8 +1,1 @@
-if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
-	meta = document.createElement('meta');
-	meta.name = "viewport";
-	meta.content = "initial-scale = 1.0, user-scalable = no"
-	document.getElementsByTagName('head').item(0).appendChild(meta);
-	$("input").append(' autocorrect="off" autocapitalize="off" ');
-}
-
+if((navigator.userAgent.indexOf('iPad')!=-1)){meta=document.createElement('meta');meta.name="viewport";meta.content="initial-scale = 1.0, user-scalable = no"document.getElementsByTagName('head').item(0).appendChild(meta);$("input").append(' autocorrect="off" autocapitalize="off" ')}
