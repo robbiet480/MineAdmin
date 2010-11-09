8891662886c794ef390944458222b55c164589bc
Dennis Tang <dtang@ismcreative.net>
2010-11-08 21:05:59 -0800

    disable autocorrect/capitalize for iPad

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -3,5 +3,6 @@ if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
 	meta.name = "viewport";
 	meta.content = "initial-scale = 1.0, user-scalable = no"
 	document.getElementsByTagName('head').item(0).appendChild(meta);
+	$("input").append(' autocorrect="off" autocapitalize="off" ');
 }
 
