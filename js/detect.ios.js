440f2850a87f1381c586f6909801550d91494b4b
Dennis Tang <dtang@ismcreative.net>
2010-11-08 20:48:25 -0800

    extra quotes

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -2,7 +2,7 @@
   if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
 	meta = document.createElement('meta');
 	meta.name = "viewport";
-	meta.content = "initial-scale = 1.0, user-scalable = "no""
+	meta.content = "initial-scale = 1.0, user-scalable = no"
 	document.getElementsByTagName('head').item(0).appendChild(meta);
   }
 </script>
