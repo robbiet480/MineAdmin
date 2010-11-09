a7184bee46766c8b0f7afcc3c6cd3259ca619457
Dennis Tang <dtang@ismcreative.net>
2010-11-08 23:55:49 -0800

    ipad startup fix

--- js/detect.ios.js
+++ js/detect.ios.js
@@ -1,3 +1,9 @@
 if ( (navigator.userAgent.indexOf('iPad') != -1) ) {
-	document.write("<meta name="viewport" content="initial-scale = 1.0, user-scalable = no" /><link rel="apple-touch-startup-image" href="images/ipad-startup.png" />");
+	meta = document.createElement('meta');
+	meta.name = "viewport";
+	meta.content = "initial-scale = 1.0, user-scalable = no"
+	link = document.createElement('link');
+	link.rel = "apple-touch-startup-image";
+	link.href = "images/ipad-startup,png";
+	document.getElementsByTagName('head').item(0).appendChild(meta + link);
 }
