ebc0cd98bb45ad09b02a117f53188bbd3cd1d6c3
Dennis Tang <dtang@ismcreative.net>
2010-11-09 21:15:24 -0800

    finished items page

--- items.php
+++ items.php
@@ -1,16 +1,27 @@
 <?php
-$pageid = "news";
+$pageid = "items";
 require_once('header_inc.php');
 require_once('includes/header.php');
 ?>
+
 	<div id="page_wrap">
-		<?php
-		$items = $minecraft->item_list();
-		foreach ($items as $item) {
-			echo '<img src="images/'.$item['itemid'].'.png">';
-			echo '<input type="text" name="id_'.$item['itemid'].'" value="'.$item['itemid'].'" <br />';
-			echo '<input type="text" name="name_'.$item['itemid'].'" value="'.$item['name'].'" <br /><br />';
-		}
-		?>
+
+		<h1>Items</h1>
+		
+        <div id="items">
+			<?php
+			$items = $minecraft->item_list();
+			foreach ($items as $item) {
+				echo '<label style="clear:both;"><span><img src="images/'.$item['itemid'].'.png" width="25px" height="25px" /><input type="text" class="item_id" name="id_'.$item['itemid'].'" value="'.$item['itemid'].'" />';
+				echo '<input type="text" class="item_name" name="name_'.$item['itemid'].'" value="'.$item['name'].'" /></span></label><br />';
+			}
+			?>
+        </div>
+			
+		<div>
+			<form action="#" method="POST">
+			<input class="button" type="submit">
+			</form>
+		</div>
 	</div>
 <?php require_once('includes/footer.php'); ?>
