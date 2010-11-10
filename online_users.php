52b1abaace6813ef17ff6a934038b6b97696154b
Robbie Trencheny <me@robbiet.us>
2010-11-09 17:33:08 -0800

    fuxing php

--- online_users.php
+++ online_users.php
@@ -22,13 +22,13 @@ if(empty($list)) {
 foreach($list as $player){
 ?>
 <ul>
-        <li class="name"><?php echo $player['name'];?></li>
-        <li class="group"><?php $grp=$minecraft->group_prefix($player['prefix']);  echo $grp['name'];?></li>
-        <li class="build"><?php if($player['canBuild']){ echo "<img src=\"images/icons/accept.png\" />"; }else{ echo "<img src=\"images/toolbar-icon-delete.png\" />"; }?></li>
-        <li class="world"><?php if($player['canModifyWorld']){ echo "<img src=\"images/icons/accept.png\" />"; }else{ echo "<img src=\"images/toolbar-icon-delete.png\" />"; };?></li>
-        <li class="ip"><?php if($logged){ echo $player['ip']; }else{ echo "HIDDEN"; };?></li>
-        <li class="hand"><?php if($player['itemInHand']!="-1"){ echo "<img src=\"images/".$player['itemInHand'].".png\" title=\"".$minecraft->get_item_name($player['itemInHand'])."\" />"; }else{ echo "o"; }?></li>
-        <li class="actions"><a href="javascript:kick_player('<?php echo $player['name'];?>');">&lsaquo; kick &rsaquo;</a> <!--<a href="javascript:ban_player('<?php echo $player['name'];?>','<?php echo $player['ip'];?>');">ban</a>//--> <a href="javascript:inv_player('<?$
+	<li class="name"><?php echo $player['name'];?></li>
+	<li class="group"><?php $grp=$minecraft->group_prefix($player['prefix']);  echo $grp['name'];?></li>
+	<li class="build"><?php if($player['canBuild']){ echo "<img src=\"images/icons/accept.png\" />"; }else{ echo "<img src=\"images/toolbar-icon-delete.png\" />"; }?></li>
+	<li class="world"><?php if($player['canModifyWorld']){ echo "<img src=\"images/icons/accept.png\" />"; }else{ echo "<img src=\"images/toolbar-icon-delete.png\" />"; };?></li>
+	<li class="ip"><?php if($logged){ echo $player['ip']; }else{ echo "HIDDEN"; };?></li>
+	<li class="hand"><?php if($player['itemInHand']!="-1"){ echo "<img src=\"images/".$player['itemInHand'].".png\" title=\"".$minecraft->get_item_name($player['itemInHand'])."\" />"; }else{ echo "o"; }?></li>
+	<li class="actions"><a href="javascript:kick_player('<?php echo $player['name'];?>');">&lsaquo; kick &rsaquo;</a> <!--<a href="javascript:ban_player('<?php echo $player['name'];?>','<?php echo $player['ip'];?>');">ban</a>//--> <a href="javascript:inv_player('<?php echo $player['name'];?>');">&lsaquo; inventory &rsaquo;</a> <a href="javascript:msg_player('<?php echo $player['name'];?>');">&lsaquo; msg &rsaquo;</a></li>
 </ul>
 <?php
 }
