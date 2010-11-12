6dd4dc880eaa69b38ee22a186fea965913597111
Robbie Trencheny <me@robbiet.us>
2010-11-11 19:21:08 -0800

    fixed deploy to grab submodules

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,16 @@
-set :application, "MineAdmin"
+set :application, "MCSAdmin"
 set :repository,  "git@github.com:robbiet480/MCSAdmin.git"
-
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/"
-
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-
+set :git_enable_submodules, 1
 role :app, "67.228.153.12"
 role :web, "67.228.153.12"
 set :use_sudo, false
-
-
 set :user, "root"
 set :password, "DC91652D1695606F425FBDAA017E357DC4026DD3CFF04D49CCCB94BCC583BFFA"
 
