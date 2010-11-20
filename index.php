bcde12928947c2ed2344ec94f8bfab5800d4b857
Robbie Trencheny <me@robbiet.us>
2010-11-19 21:39:09 -0800

    wouldve also been nice to ACTUALLY OUTPUT MARKDOWN. *headdesk*

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include('admin/includes/markdown.php');
+
+?>
 <!doctype html>
 <html>
 <head>
@@ -22,7 +26,7 @@
    <div id="wrapper">
        <div id="container">
                <div id="content">
-<?php echo file_get_contents('admin/markdown.md');?>
+<?php echo Markdown(file_get_contents('admin/markdown.md'));?>
 	            </div>
                <div id="footer">Powered by <a href="http://minecraftservers.com">MinecraftServers.com</a></div>
        </div>
