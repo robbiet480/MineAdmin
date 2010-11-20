78f74303a580bed18351224653c47a0c481e04da
Dennis Tang <dtang@ismcreative.net>
2010-11-19 20:20:54 -0800

    improved landing page

--- index.php
+++ index.php
@@ -7,25 +7,21 @@
     #wrapper{display:table;overflow:hidden;margin:0px auto;}
     *:first-child+html #wrapper {position:relative;}/*ie7*/
     * html #wrapper{position:relative;}/*ie6*/
-    
-    #container{display:table-cell;vertical-align: middle;}
-    *:first-child+html #container{position:absolute;top:50%;}/*ie7*/
-    * html #container{position:absolute;top:50%;}/*ie6*/
-    
-    *:first-child+html #content{position:relative;top:-50%;}/*ie7*/
-    * html #content{position:relative;top:-50%;}/*ie6*/
-    
+    #container{display:table-cell;}
     html,body{height:100%;}
-    body {background: #b2b2b2 url('images/bg.png') no-repeat top center;}
+    body {background: #d3d3d3 url('images/bg.png') no-repeat top center; font-family: sans-serif; font-size: 14px;}
     #wrapper{height:100%;width:960px;}
+    #content {margin-top: 100px;}
+    #markdown {margin-top: 50px;}
+    #logo {background: #972e2e url('images/logo.png') no-repeat center center;text-indent: -9999px;height:55px;width:499px;}
   </style>
 </head>
 <body>
    <div id="wrapper">
        <div id="container">
                <div id="content">
-                     <h1>MinecraftServers.com</h1>
-                     <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</div>
+               	<div><a href="http://minecraftservers.com/" id="logo">MinecraftServers.com</a></div>
+                     <div id="markdown">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</div>
                </div>
        </div>
   </div> 
