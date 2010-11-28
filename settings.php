8ee2e186ea8325f26842d9b2bb7777ee392a5428
TheCrazyT <NoName@.(none)>
2010-11-28 10:31:40 +0100

    Move menu out of the files into a function in header_inc.php Makes more sense, because its less work if you want to modify the menu.

--- settings.php
+++ settings.php
@@ -6,41 +6,8 @@ if($_GET['save']=="1"){
 $config=$minecraft->configuration_files();
 //echo "Coming soon....";
 //exit;
+head("Administration Settings");
 ?>
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<title>Administration Settings</title>
-	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.alerts.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="jquery.autocomplete.css" type="text/css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.2.css" media="screen" />
-	<link rel="stylesheet" href="jquery.jgrowl.css" type="text/css" media="screen" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery-css-transform.js"></script>
-	<script type="text/javascript" src="jquery.autocomplete.pack.js"></script>
-	<script type="text/javascript" src="jquery.bgiframe.min.js"></script>
-	<script type="text/javascript" src="jquery.alerts.js"></script>
-        <script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-	<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.2.js"></script>
-	<script type="text/javascript" src="jquery.jgrowl_minimized.js"></script>
-	<script type="text/javascript" src="jquery.ui.all.js"></script>
-	<script type="text/javascript" src="system.js"></script> 
-</head>
-<body>
-	<div id="menu-wrap">
-		<h2>MineCraft Server</h2>
-		<ul>
-			<li><a href="logout_process.php">Logout</a></li>
-			<li><a href="settings.php">Settings</a></li>
-                        <li><a href="backup.php">Backup</a></li>
-			<li><a href="exec.php">Exec</a></li>
-			<li><a href="groups.php">Groups</a></li>
-			<li><a href="bans.php">Bans</a></li>
-			<li><a href="users.php">Users</a></li>
-			<li><a href="start.php">Online Now</a></li>
-		</ul>
-	</div>
 	<div id="page_wrap">
 		<div id="online_wrap">
                     <form method="post" action="settings.php?save=1">
