6ba53c32f127423337d1fb51362fa206659fc89c
TheCrazyT <NoName@.(none)>
2011-01-02 11:41:19 +0100

    fixed logout bug.

--- logout_process.php
+++ logout_process.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if($_SESSION['user']==""){
-	header("Location: /login.php");
+	header("Location: login.php");
 	exit;
 }
 unset($_SESSION['user']);
-session_destroy();
-header("Location: /index.php");
+@session_destroy();
+header("Location: index.php");
 ?>
