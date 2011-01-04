394bb61d3051c033f074d30c72cf30d853307c36
Emirin <thetrainable@gmail.com>
2011-01-03 16:11:53 -0800

    Added back the /admin directory and placed the index page back to where it was.  Made sure all links to the Markdown were working.

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php
+if(!file_exists('config.php')) {
+	header("Location: install.php");
+}
+if(file_exists('install.php')) {
+	unlink('install.php');
+}
+session_start();
+if($_SESSION['user']==""){
+	header("Location: login.php");
+	exit;
+}else{
+	header("Location: start.php");
+}
+?>
