bbb8541de850be102c198da073dd1d5d5b7a80d6
resba <thatwebguymatt@gmail.com>
2011-02-23 15:57:42 -0500

    quick update

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ include('admin/includes/markdown.php');
    <div id="wrapper">
        <div id="container">
                <div id="content">
-<?php echo Markdown(file_get_contents('admin/markdown.md'));?>
+<?php echo markdown(file_get_contents('admin/markdown.md'));?>
 	            </div>
                <div id="footer">Powered by <a href="http://minecraftservers.com">MinecraftServers.com</a></div>
        </div>
