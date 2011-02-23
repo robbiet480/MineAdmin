81aac2c366e428db57cead4f16e1e343373e5c4f
resba <thatwebguymatt@gmail.com>
2011-02-23 16:04:22 -0500

    Readded index.php and seperated stylesheet

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+if(!file_exists('admin/config.php')) {
+	header("Location: admin/install.php");
+}
+include('admin/includes/markdown.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>MinecraftServers.com</title><link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+   <div id="wrapper">
+       <div id="container">
+               <div id="content">
+					<?php echo Markdown(file_get_contents('admin/markdown.md'));?>
+	            </div>
+               <div id="footer">Powered by <a href="http://minecraftservers.com">MinecraftServers.com</a></div>
+       </div>
+  </div> 
+</body>
+</html>
